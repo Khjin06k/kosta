3b55abc13eecd2d062577c015f80a6d950f2a572
gmlwls <khj4209kk@gmail.com>
2023-08-18 10:07:40 +0900

    day6. chap5 추가 연습 문제

--- src/practice/chap5/p12.java
+++ src/practice/chap5/p12.java
@@ -6,7 +6,7 @@ public class p12 {
         // 달팽이는 낮동안 52cm를 올라올 수 있다.
         // 날이 지면 잠을 자야한다.
         // 자는 동안 미끄러져 3cm가 내려간다.
-        // 달팽이가 다 올라오기 위해서 몇일이 필요한가? >> 7일
+        // 달팽이가 다 올라오기 위해서 몇일이 필요한가? >> 6일
         int day = 0;
         int m = 0;
         while(m<300){
@@ -15,6 +15,20 @@ public class p12 {
             if(m >= 300) break;
             else m -= 3;
         }
+
+        /*
+        int depth = 3*100, height = 0;;
+        final int up = 55;
+        final int down = 3;
+        int day = 0;
+        while(true){
+            height += up;
+            day++;
+            if(height>=depth) break;
+            height-=down;
+         }
+        */
+
         System.out.println(day);
     }
 }

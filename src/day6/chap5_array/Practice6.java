e15bd0ba24ff405c28d35d3493a3e786fb8fe7d9
gmlwls <khj4209kk@gmail.com>
2023-08-18 15:33:24 +0900

    day6. chap5 추가 연습 문제(배열의 첫 행은 1로 초기화 한 후 위와 왼쪽의 숫자를 더해서 배열에 저장 및 출력)

--- src/day6/chap5_array/Practice6.java
+++ src/day6/chap5_array/Practice6.java
@@ -2,25 +2,24 @@ package day6.chap5_array;
 
 public class Practice6 {
     public static void main(String[] args) {
-        // 배열을 모두 1로 초기화 한 후 다음부터는 위와 왼쪽의 숫자를 더해서 배열에 저장하는 코드 작성
+        // 첫번째 행을 모두 1로 초기화 한 후 다음부터는 위와 왼쪽의 숫자를 더해서 배열에 저장하는 코드 작성
         int[][] arr = new int[5][5];
-        // 배열의 요소를 1로 초기화
-        for(int i = 0; i<arr.length; i++){
-            for(int j = 0; j<arr.length; j++){
-                arr[i][j] = 1;
-            }
+        // 첫 번째 행 배열의 요소를 1로 초기화
+        for(int i = 0; i<arr[0].length; i++){
+                arr[0][i] = 1;
         }
 
         // 왼쪽과 위의 숫자를 더해서 배열에 저장
         for(int i = 1; i<arr.length; i++){
-            for(int j = 1; j<arr.length; j++){
-                arr[i][j] = arr[i-1][j]+arr[i][j-1];
+            for(int j = 0; j<arr[i].length; j++){
+                if(j==0) arr[i][j] = arr[i-1][j]; // 맨 왼쪽은 왼쪽이 없기 때문에 위의 요소만 더하면 됨
+                else arr[i][j] = arr[i-1][j]+arr[i][j-1];
             }
         }
 
         // 배열 출력
         for(int i = 0; i<arr.length; i++){
-            for(int j = 0; j<arr.length; j++){
+            for(int j = 0; j<arr[i].length; j++){
                 System.out.print(arr[i][j]+"\t");
             }
             System.out.println();

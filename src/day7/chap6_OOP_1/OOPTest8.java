15aee709e2844e450c17da03e83e20e30d140713
gmlwls <khj4209kk@gmail.com>
2023-08-21 15:34:54 +0900

    day7. chap6 메서드 연습

--- src/day7/chap6_OOP_1/OOPTest8.java
+++ src/day7/chap6_OOP_1/OOPTest8.java
@@ -1,4 +1,30 @@
 package day7.chap6_OOP_1;
 
+class Data{
+    int x;
+
+}
+
 public class OOPTest8 {
+    static void increment(int x) {
+        x += 1;
+        System.out.println(x);
+    }
+
+    static void increment2(Data d){
+        d.x +=1;
+        System.out.println(d.x);
+    }
+
+    public static void main(String[] args) {
+        Data data = new Data();
+        data.x = 10;
+
+        increment(data.x); // 11
+
+        System.out.println(data.x); // 10
+        increment2(data); // 11
+        System.out.println(data.x); // 11
+
+    }
 }

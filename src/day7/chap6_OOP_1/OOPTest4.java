1f00a8c994974b94932c443c3d25f46d04025543
gmlwls <khj4209kk@gmail.com>
2023-08-21 12:39:28 +0900

    day7. chap6 변수의 종류

--- src/day7/chap6_OOP_1/OOPTest4.java
+++ src/day7/chap6_OOP_1/OOPTest4.java
@@ -3,7 +3,7 @@ package day7.chap6_OOP_1;
 // 지역변수는 메서드 영역에서 선언, 클래스 변수와 인스턴스 변수는 클래스 영역에서 선언
 class MyClass{
     int num; // 인스턴스 변수 >> 인스턴스 생성 시 생성
-    static int snum; // 클래스 변수 (static 변수, 공유 변수) >> 클래스가 메모리에 올라갈 때 생성
+    static int snum; // 클래스 변수 (static 변수, 공유 변수) >> 클래스가 메모리에 올라갈 때 생성되며 올라갈 때 하나만 만들어짐
 
     void setNum(int n){ // 지역변수와 동일함 >> 호출되는 시점에 생성되었다가 사라지며, Stack 영역
 

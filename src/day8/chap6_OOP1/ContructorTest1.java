b7b91399b503a4d21742fa3b7a974c8694107af0
gmlwls <khj4209kk@gmail.com>
2023-08-22 11:41:31 +0900

    day8. chap6 생성자 및 생성자 내 생성자 호출 this()

--- src/day8/chap6_OOP1/ContructorTest1.java
+++ src/day8/chap6_OOP1/ContructorTest1.java
@@ -9,6 +9,10 @@ class Person1{
     // 생성자도 오버로딩이 가능함
     Person1(){
        // System.out.println("Person()");
+        // 기본 생성자는 0, null  등으로 초기와가 되는데 기본 생성자 생성을 변경할 수 있음
+        this("none", 0); // 생성자에서 다른 생성자를 호출 >> String, int 매개변수이므로 Person(String n, int a)를 불러옴
+        /*age = 0;
+        name = "none";*/
     } // 기본 생성자, 생성자는 리턴타입이 없으며, void가 생략
 
     Person1(String n, int a){ // 생성자가 있다면 기본생성자가 자동생성되지 않아서 위처럼 생성을 해줘야 함
@@ -17,11 +21,16 @@ class Person1{
     }
 
     Person1(String n){
-        name = n;
+        this(n, 0); // 생성자에서 다른 생성자를 호출 >> String, int 매개변수이므로 Person(String n, int a)를 불러옴
+        System.out.println(n); // 생성자에서 생성자를 호출할 때에는 this()가 맨 앞에 와야 하기 때문에 그 외의 코드는 모두 this() 뒤에 와야 함
+        /*name = n;
+        age = 0;*/
     }
 
     Person1(int a){
-        age = a;
+        this("none", a); // 생성자에서 다른 생성자를 호출 >> String, int 매개변수이므로 Person(String n, int a)를 불러옴
+        /*age = a;
+        name = "none";*/
     }
 }
 public class ContructorTest1 {

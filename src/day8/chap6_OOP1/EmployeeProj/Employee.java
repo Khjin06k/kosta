d8711eeac58ed0fdefc56d07cccb8136cd3bbcbf
gmlwls <khj4209kk@gmail.com>
2023-08-22 17:00:29 +0900

    day8. chap6 과제

---
+++ src/day8/chap6_OOP1/EmployeeProj/Employee.java
@@ -0,0 +1,16 @@
+package day8.chap6_OOP1.EmployeeProj;
+
+public class Employee {
+    int id;
+    String name;
+    String dep;
+    long pay;
+
+    Employee(int id, String name, String dep, long pay){
+        this.id = id;
+        this.name = name;
+        this.dep = dep;
+        this.pay = pay;
+    }
+
+}

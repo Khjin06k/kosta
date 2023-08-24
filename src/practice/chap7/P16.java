b008d1607bf3104c143eafc0b0b54ae816333c0a
gmlwls <khj4209kk@gmail.com>
2023-08-24 14:35:04 +0900

    day10. chap7 연습문제

---
+++ src/practice/chap7/P16.java
@@ -0,0 +1,16 @@
+package practice.chap7;
+
+public class P16 {
+    // 다음 중 연산결과가 true가 아닌 것은? (모두 고르시오) b, e
+
+    class Car {}
+    class FireEngine extends Car{} //implements Movable {}
+    class Ambulance extends Car {}
+    FireEngine fe = new FireEngine();
+
+    // a. fe instanceof FireEngine
+    // b. fe instanceof Movable ...
+    //c. fe instanceof Object
+    //d. fe instanceof Car
+    //e. fe instanceof Ambulance
+}

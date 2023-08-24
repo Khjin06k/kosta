4c6fed565ce6dabdfdcbf417a402e9072228fec9
gmlwls <khj4209kk@gmail.com>
2023-08-24 15:04:20 +0900

    day10. chap7 연습문제

---
+++ src/practice/chap7/p8_9.java
@@ -0,0 +1,16 @@
+package practice.chap7;
+
+public class p8_9 {
+    // [7-8] 다음 중 접근제어자를 접근범위가 넓은 것에서 좁은 것의 순으로 바르게 나열한 것은? a
+    //a. public-protected-(default)-private
+    // b. public-(default)-protected-private
+    // c. (default)-public-protected-private
+    // d. private-protected-(default)-public
+
+    //[7-9] 다음 중 제어자 final을 붙일 수 있는 대상과 붙였을 때 그 의미를 적은 것이다.
+    // 옳지 않은 것은? (모두 고르시오) b
+    //a. 지역변수 - 값을 변경할 수 없다.
+    //b. 클래스 - 클래스에 새로운 멤버를 추가할 수 없다. // 상속을 할 수 없음을 의미
+    // c. 메서드 - 오버로딩을 할 수 없다.
+    //d. 멤버변수 - 값을 변경할 수 없다.
+}

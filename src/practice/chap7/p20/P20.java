d0e702192e48a894d56002fe345c97d0d1723f4e
gmlwls <khj4209kk@gmail.com>
2023-08-24 17:42:57 +0900

    day10. chap7 연습문제

--- src/practice/chap7/p20/P20.java
+++ src/practice/chap7/p20/P20.java
@@ -1,13 +1,16 @@
 package practice.chap7.p20;
-// [7-20] 다음의 코드를 실행한 결과를 적으시오. 100 / "Child Mehtod" / 200 / "Child Method"
+// [7-20] 다음의 코드를 실행한 결과를 적으시오.
+// 100 / "Child Mehtod" / 200 / "Child Method"
 class Parent {
     int x=100;
     void method() { System.out.println("Parent Method");
-    } }
+    }
+}
 class Child extends Parent {
     int x=200;
     void method() { System.out.println("Child Method");
-    } }
+    }
+}
 public class P20 {
     public static void main(String[] args) {
         Parent p = new Child(); Child c = new Child();

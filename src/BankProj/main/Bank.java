949b5bc04f38c51e38431ba3fe9ebe2aefbb7551
gmlwls <khj4209kk@gmail.com>
2023-08-25 10:25:18 +0900

    day11. Bank문제 다형성 적용

--- src/BankProj/main/Bank.java
+++ src/BankProj/main/Bank.java
@@ -1,6 +1,7 @@
 package BankProj.main;
 
 import BankProj.acc.Account;
+import BankProj.acc.SpecialAccount;
 
 import java.util.Scanner;
 
@@ -23,6 +24,16 @@ public class Bank {
         return Integer.parseInt(sc.nextLine());
     }
 
+    void selAccMenu(){
+        System.out.println("[계좌 개설]");
+        System.out.println("1. 일반 계좌");
+        System.out.println("2. 특수 계좌");
+        System.out.println("선택 >> ");
+        int sel = Integer.parseInt(sc.nextLine());
+        if(sel == 1) makeAccount();
+        else makeSpecialAccount();
+    }
+
     // 계좌 개설
     void makeAccount(){
         System.out.println("[계좌 개설]");
@@ -34,6 +45,24 @@ public class Bank {
         int money = Integer.parseInt(sc.nextLine());
         if(accCnt == accs.length) System.out.println("개설 가능한 계좌 수가 넘어 개설 불가능합니다.");
         else accs[accCnt++] = new Account(id, name, money);
+    }
+    // 특수 계좌 개설
+    void makeSpecialAccount(){
+        System.out.println("[특수 계좌 개설]");
+        System.out.print("계좌 번호 : ");
+        String id = sc.nextLine();
+        System.out.print("이름 : ");
+        String name = sc.nextLine();
+        System.out.print("입금액 : ");
+        int money = Integer.parseInt(sc.nextLine());
+        System.out.print("등급(VIP-V, Gold-G, Silver-S, Normal-N) : ");
+        String grade = sc.nextLine();
+        // 등급을 잘못 입력 받았을 경우 잘못된 등급임을 알리기
+        if(!grade.equals("V") && !grade.equals("G") && !grade.equals("S") && !grade.equals("N")){
+            System.out.println("잘못된 등급입니다.");
+        }
+        if(accCnt == accs.length) System.out.println("개설 가능한 계좌 수가 넘어 개설 불가능합니다.");
+        else accs[accCnt++] = new SpecialAccount(id, name, money, grade); // 다형성 이용하여 부모 클래스 타입의 배열에 생성 가능
     }
 
     // 계좌번호에 따른 사용자 조회
@@ -57,7 +86,23 @@ public class Bank {
         }
         System.out.print("입금액 : ");
         int money = Integer.parseInt(sc.nextLine());
-        acc.deposit(money);
+        if(acc instanceof SpecialAccount){
+            switch(((SpecialAccount) acc).getGrade()){
+                case "V" :
+                    money = (int)(money*1.04);
+                    break;
+                case "G" :
+                    money = (int)(money*1.03);
+                    break;
+                case "S" :
+                    money = (int)(money*1.02);
+                    break;
+                case "N" :
+                    money = (int)(money*1.01);
+                    break;
+            }
+            acc.deposit(money);
+        } else acc.deposit(money);
     }
 
     // 출금
@@ -89,6 +134,10 @@ public class Bank {
     // 전체 계좌 조회
     void allAccountInfo(){
         for(int i = 0; i<accCnt; i++){ // 전체 배열을 돌릴 필요 없이 값이 존재하는 곳 까지만 반복을 돌리기 위해 accCnt 만큼만 반복 진행
+            if(accs[i] instanceof SpecialAccount){
+                System.out.println("계좌번호 : " + accs[i].getId() + ", 이름 : " + accs[i].getName() + ", 잔액 : " + accs[i].getBalance() + ", 등급 : " + ((SpecialAccount) accs[i]).getGrade());
+
+            }
             System.out.println("계좌번호 : " + accs[i].getId() + ", 이름 : " + accs[i].getName() + ", 잔액 : " + accs[i].getBalance());
         }
     }
@@ -103,7 +152,7 @@ public class Bank {
             if(sel==0) break;
             switch(sel) {
                 case 1:
-                    bank.makeAccount();
+                    bank.selAccMenu();
                     break;
                 case 2:
                     bank.deposit();

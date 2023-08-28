1b09aec2a36a1ae0361ecf66a693cd5aed1ae088
gmlwls <khj4209kk@gmail.com>
2023-08-28 14:55:58 +0900

    day12. chap8. 연습문제

--- src/practice/chap8/P9.java
+++ src/practice/chap8/P9.java
@@ -46,8 +46,10 @@ class UnsupportedFuctionException extends RuntimeException{
 public class P9 {
 
     public static void main(String[] args) throws Exception {
-
-        throw new UnsupportedFuctionException("지원하지 않는 기능입니다.", 100);
-
+        //try{
+            throw new UnsupportedFuctionException("지원하지 않는 기능입니다.", 100);
+        //}catch (UnsupportedFuctionException e){
+        //   System.out.println(e.getMessage());
+        //}
     }
 }

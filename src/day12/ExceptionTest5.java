fb5520d62874d8352b3f257809ef229674a2a9fd
gmlwls <khj4209kk@gmail.com>
2023-08-28 13:38:49 +0900

    day12. chap8. 예외처리

---
+++ src/day12/ExceptionTest5.java
@@ -0,0 +1,8 @@
+package day12;
+
+public class ExceptionTest5 {
+    public static void main(String[] args) {
+        String nstr = "123o";
+        //int n = Integer.parseInt(nstr); // 문자열을 int로 변경 >> 에러 발생
+    }
+}
---
+++ src/day12/ExceptionTest6.java
@@ -0,0 +1,28 @@
+package day12;
+
+import java.io.IOException;
+
+public class ExceptionTest6 {
+    static void method() throws IOException, Exception{
+        boolean flag = false;
+        if(flag){
+            throw new IOException("입출력 예외");
+        }else{
+            throw new Exception("모든 예외");
+        }
+
+    }
+    public static void main(String[] args) {
+        try{
+            method();
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+            //e.printStackTrace();
+            //e.getMessage();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            //e.getMessage();
+            //e.printStackTrace();
+        }
+    }
+}

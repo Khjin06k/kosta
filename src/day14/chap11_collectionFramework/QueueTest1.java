3a45bf0ffb97c761b7f9dc906345ebe8b77dbc4a
gmlwls <khj4209kk@gmail.com>
2023-08-30 15:36:42 +0900

    day14. chap11. 스택, 큐 예제

---
+++ src/day14/chap11_collectionFramework/QueueTest1.java
@@ -0,0 +1,19 @@
+package day14.chap11_collectionFramework;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueTest1 {
+    public static void main(String[] args) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(100); // enqueue
+        queue.offer(200); // enqueue
+        queue.offer(300); // enqueue
+        System.out.println(queue.poll()); // dequeue
+        System.out.println(queue.poll());  // dequeque
+        System.out.println(queue.remove()); // dequeque
+
+        queue.clear(); // 초기화
+
+    }
+}

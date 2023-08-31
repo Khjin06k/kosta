7ede24371aad4394bd14a6d2d4390fd06f3b0358
gmlwls <khj4209kk@gmail.com>
2023-08-31 16:49:24 +0900

    day15. chap14. 바이트 기반 입출력, 2개 이상의 파일을 합치는 경우

--- src/day15/chap12_inputOutput/SequenceStreamTest1.java
+++ src/day15/chap12_inputOutput/SequenceStreamTest1.java
@@ -1,9 +1,7 @@
 package day15.chap12_inputOutput;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.SequenceInputStream;
+import java.io.*;
+import java.util.Vector;
 
 public class SequenceStreamTest1 {
     public static void main(String[] args) {
@@ -13,10 +11,19 @@ public class SequenceStreamTest1 {
 
         SequenceInputStream sis = null;
 
+        Vector<InputStream> v = new Vector<>();
+
         try{
-            fis1 = new FileInputStream("news.txt");
+
+            /*fis1 = new FileInputStream("news.txt");
             fis2 = new FileInputStream("news2.txt");
-            sis = new SequenceInputStream(fis1, fis2); // 여러개의 파일은 Vector에 넣어서 Enumeration을 이용해 넣어주면 됨
+            sis = new SequenceInputStream(fis1, fis2); // 여러개의 파일은 Vector에 넣어서 Enumeration을 이용해 넣어주면 됨*/
+
+            // vector 이용 >> 2개 이상의 여러개의 파일을 합칠 수 있음
+            v.add(new FileInputStream("news.txt"));
+            v.add(new FileInputStream("news2.txt"));
+            sis = new SequenceInputStream(v.elements());
+
             fos = new FileOutputStream("total.txt");
 
             byte[] buff = new byte[1024];
@@ -28,7 +35,7 @@ public class SequenceStreamTest1 {
             e.printStackTrace();
         }finally {
             try{
-                if(sis != null) sis.close(); // 보조스트림을 닫으면 기본 스트림도 닫힘
+                if(sis != null) sis.close(); // 보조스트림을 닫으면 기본 스트림도 닫
                 if(fos != null) fos.close();
             }catch (IOException e){
                 e.printStackTrace();

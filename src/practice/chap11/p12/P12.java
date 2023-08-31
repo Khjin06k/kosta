7bf8d382eef76695c6ad8746440ca68654783696
gmlwls <khj4209kk@gmail.com>
2023-08-31 12:23:46 +0900

    day15. chap11. 연습문제

--- src/practice/chap11/p12/P12.java
+++ src/practice/chap11/p12/P12.java
@@ -11,17 +11,19 @@ import java.util.HashMap;
 class SutdaDeck {
     final int CARD_NUM = 20;
     SutdaCard[] cards = new SutdaCard[CARD_NUM];
-    int pos=0;//다음에 가져올 카드의 위치
+    int pos = 0;//다음에 가져올 카드의 위치
     HashMap jokbo = new HashMap(); // 족보를 저장할 HashMap
+
     SutdaDeck() {
-        for(int i=0;i < cards.length;i++) {
-            int num = i%10+1;
-            boolean isKwang = i < 10 && (num==1 || num==3 || num==8);
-            cards[i] = new SutdaCard(num,isKwang);
+        for (int i = 0; i < cards.length; i++) {
+            int num = i % 10 + 1;
+            boolean isKwang = i < 10 && (num == 1 || num == 3 || num == 8);
+            cards[i] = new SutdaCard(num, isKwang);
         }
 
         registerJokbo(); // 족보를 등록한다.
     }
+
     void registerJokbo() {
             /*
             (1) 아래의 로직에 맞게 코드를 작성하시오.
@@ -53,14 +55,13 @@ class SutdaDeck {
         jokbo.put("64", 20610);
 
 
-
     }
 
     int getPoint(Player p) {
-            if(p==null) return 0;
-            SutdaCard c1 = p.c1;
-            SutdaCard c2 = p.c2;
-            Integer result = 0;
+        if (p == null) return 0;
+        SutdaCard c1 = p.c1;
+        SutdaCard c2 = p.c2;
+        Integer result = 0;
             /*
             (2) 아래의 로직에 맞게 코드를 작성하시오.
             1. 카드 두 장이 모두 광이면, jokbo에서 키를 "KK"로 해서 점수를 조회한다.
@@ -69,67 +70,84 @@ class SutdaDeck {
             (c1.num + c2.num) % 10 + 1000
             4.Player의 점수(point)에 계산한 값을 저장한다.
             */
-        String key = c1.num+""+c2.num;
-            if(c1.isKwang && c2.isKwang){
-                result = (Integer) jokbo.get("KK");
-            }
-            else if(jokbo.containsKey(key)){
-                result = (Integer)jokbo.get(key);
-            }else{
+        String key = c1.num + "" + c2.num;
+        if (c1.isKwang && c2.isKwang) {
+            result = (Integer) jokbo.get("KK");
+        } else {
+            if (jokbo.containsKey(key)) {
+                result = (Integer) jokbo.get(key);
+            } else {
                 result = (c1.num + c2.num) % 10 + 1000;
             }
-            return result.intValue();
+        }
+        return result.intValue();
     }
+
     SutdaCard pick() throws Exception {
-            SutdaCard c = null;
-            if(0 <= pos && pos < CARD_NUM) { c = cards[pos];
-                cards[pos++] = null; }else{
-                throw new Exception("남아있는 카드가 없습니다."); }
-            return c;
+        SutdaCard c = null;
+        if (0 <= pos && pos < CARD_NUM) {
+            c = cards[pos];
+            cards[pos++] = null;
+        } else {
+            throw new Exception("남아있는 카드가 없습니다.");
+        }
+        return c;
     }
+
     void shuffle() {
-            for(int x=0; x < CARD_NUM * 2; x++) {
-                int i = (int)(Math.random() * CARD_NUM);
-                int j = (int)(Math.random() * CARD_NUM);
-                SutdaCard tmp = cards[i];
-                cards[i] = cards[j]; cards[j] = tmp;
-            }
+        for (int x = 0; x < CARD_NUM * 2; x++) {
+            int i = (int) (Math.random() * CARD_NUM);
+            int j = (int) (Math.random() * CARD_NUM);
+            SutdaCard tmp = cards[i];
+            cards[i] = cards[j];
+            cards[j] = tmp;
+        }
     }
 }
+
 class Player {
     String name;
     SutdaCard c1;
     SutdaCard c2;
     int point;//카드의 등급에 따른 점수-새로 추가
-    Player(String name, SutdaCard c1, SutdaCard c2) { this.name = name ;
-        this.c1 = c1 ;
-        this.c2 = c2 ;
+
+    Player(String name, SutdaCard c1, SutdaCard c2) {
+        this.name = name;
+        this.c1 = c1;
+        this.c2 = c2;
     }
+
     public String toString() {
-        return "["+name+"]"+ c1.toString() +","+ c2.toString();
+        return "[" + name + "]" + c1.toString() + "," + c2.toString();
     }
 } // class Player
 
 class SutdaCard {
     int num;
     boolean isKwang;
+
     SutdaCard() {
         this(1, true);
     }
+
     SutdaCard(int num, boolean isKwang) {
         this.num = num;
         this.isKwang = isKwang;
     }
+
     public String toString() {
-        return num + ( isKwang ? "K":"");
+        return num + (isKwang ? "K" : "");
     }
 }
+
 public class P12 {
     public static void main(String args[]) throws Exception {
         SutdaDeck deck = new SutdaDeck();
         deck.shuffle();
-        Player p1 = new Player("타짜", deck.pick(), deck.pick()); Player p2 = new Player("고수", deck.pick(), deck.pick());
-        System.out.println(p1+" "+deck.getPoint(p1));
-        System.out.println(p2+" "+deck.getPoint(p2)); }
+        Player p1 = new Player("타짜", deck.pick(), deck.pick());
+        Player p2 = new Player("고수", deck.pick(), deck.pick());
+        System.out.println(p1 + " " + deck.getPoint(p1));
+        System.out.println(p2 + " " + deck.getPoint(p2));
+    }
 
 }

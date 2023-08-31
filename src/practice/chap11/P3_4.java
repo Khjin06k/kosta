06992eab1144e4d1b3076454862b4606943c3abe
gmlwls <khj4209kk@gmail.com>
2023-08-31 10:12:11 +0900

    day15. chap11. 연습문제 설명 추가

--- src/practice/chap11/P3_4.java
+++ src/practice/chap11/P3_4.java
@@ -12,7 +12,7 @@ public class P3_4 {
 
         // [11-4] LinkedList 클래스는 이름과 달리 실제로는 이중 원형 연결리스트(doubly circular linked list)로 구현되어 있다.
         // LinkedList인스턴스를 생성하고 11개의 요소를 추가했을 때, 이 11개의 요소 중 접근시간(access time)이 가장 오래 걸리는 요소는 몇 번째 요소인가?
-        // 답 : 6번째 요소 (LinkedList는 앞에서부터 순환하면서 찾아야 하기 때문)
+        // 답 : 6번째 요소 (LinkedList는 앞에서부터 순환하면서 찾아야 하지만 circular라면 뒤에서 앞으로도 이동이 가능. 따라서 중간이 제일 오래 걸림)
 
     }
 }

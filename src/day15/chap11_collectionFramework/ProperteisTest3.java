e99eb16b9ea4c299404b92e8d305283c9e529f89
gmlwls <khj4209kk@gmail.com>
2023-08-31 14:43:12 +0900

    day15. chap11. Properties 예제

---
+++ src/day15/chap11_collectionFramework/ProperteisTest3.java
@@ -0,0 +1,18 @@
+package day15.chap11_collectionFramework;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ProperteisTest3 {
+    public static void main(String[] args) {
+        Properties prop1 = new Properties();
+        try{
+            prop1.load(new FileInputStream("setting.txt"));
+            System.out.println(prop1.getProperty("title"));
+            System.out.println(prop1.getProperty("content"));
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+}

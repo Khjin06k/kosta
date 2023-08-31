6b81f84fff14b6694de31a98e48d32284f5225e0
gmlwls <khj4209kk@gmail.com>
2023-08-31 16:34:21 +0900

    day15. chap14. 입출력(바이트 기반 및 바이트 기반 보조스트림)

--- src/day15/chap12_inputOutput/SequenceStreamTest1.java
+++ src/day15/chap12_inputOutput/SequenceStreamTest1.java
@@ -16,7 +16,7 @@ public class SequenceStreamTest1 {
         try{
             fis1 = new FileInputStream("news.txt");
             fis2 = new FileInputStream("news2.txt");
-            sis = new SequenceInputStream(fis1, fis2);
+            sis = new SequenceInputStream(fis1, fis2); // 여러개의 파일은 Vector에 넣어서 Enumeration을 이용해 넣어주면 됨
             fos = new FileOutputStream("total.txt");
 
             byte[] buff = new byte[1024];
@@ -28,7 +28,7 @@ public class SequenceStreamTest1 {
             e.printStackTrace();
         }finally {
             try{
-                if(sis != null) sis.close();
+                if(sis != null) sis.close(); // 보조스트림을 닫으면 기본 스트림도 닫힘
                 if(fos != null) fos.close();
             }catch (IOException e){
                 e.printStackTrace();

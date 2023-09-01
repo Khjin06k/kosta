f2a3f01465a97927f311fa75187957452f397aef
gmlwls <khj4209kk@gmail.com>
2023-09-01 14:29:33 +0900

    day16. chap14. 입출력 파일

---
+++ src/day16/FileTest2.java
@@ -0,0 +1,26 @@
+package day16;
+
+import java.io.File;
+
+public class FileTest2 {
+    public static void main(String[] args) {
+        File f = new File("/Users/gmlwls/Desktop/kosta");
+        if(!f.exists() || !f.isDirectory()){
+            return;
+        }
+        /*String[] files = f.list();
+        for(String file : files){
+            System.out.println(file);
+        }*/
+
+        File[] files1 = f.listFiles();
+        for(File file : files1){
+            // System.out.println(file.isDirectory()?"[" + file.getName()+"]":file.getName());
+            if(file.isDirectory()){
+                System.out.println("[" + file.getName()+"]");
+            }else{
+                System.out.println(file.getName());
+            }
+        }
+    }
+}
